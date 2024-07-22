95c77403599ce222edb2793a89c5649b7eea3640
GostrMao <692936194@qq.com>
2024-07-22 19:43:49 +0800

    changed quickapp

--- app/src/main/java/com/example/android/QuickApp/QuickAppActivity.java
+++ app/src/main/java/com/example/android/QuickApp/QuickAppActivity.java
@@ -18,10 +18,10 @@ import android.widget.TextView;
 import com.example.android.R;
 
 import java.util.Arrays;
+import java.util.Objects;
 
-public class QuickAppActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
-    private String protocol;
-    private String packageName;
+public class QuickAppActivity extends AppCompatActivity implements View.OnClickListener{
+    private String  protocol = "HTTP";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +29,19 @@ public class QuickAppActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_quickapp);
 
 
-        findViewById(R.id.spinner).setOnKeyListener(this);
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setSelection(Adapter.NO_SELECTION, true); //Add this line before setting listener
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                protocol = spinner.getSelectedItem().toString();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                protocol = "HTTP";
+            }
+        });
         findViewById(R.id.open_quickapp_bt).setOnClickListener(this);
-
-//        findViewById(R.id.url_et)
 
     }
 
@@ -43,50 +52,43 @@ public class QuickAppActivity extends AppCompatActivity implements View.OnClickL
         if(id == R.id.open_quickapp_bt){
             //TODO list: 临时方获获取editText中内容 -> 随时监听文本框中内容变化
             //TODO list: 添加一个选择框，直接输入包名。生成url_data
-            TextView dataEditText = findViewById(R.id.data_et);
-            String uri_data = dataEditText.getText().toString();
+            TextView dataEditText = findViewById(R.id.package_et);
+            String packageName = dataEditText.getText().toString();
+            String uri_data = getUrl(packageName);
 
+            TextView uri_tv = findViewById(R.id.uri_tv);
+            if(uri_data == null){
+                uri_tv.setText("null");
+                return;
+            }
+            uri_tv.setText(uri_data);
             //TODO list:添加标志位控制日志输出
             Log.d("wltx",uri_data);
-//            (EditText)findViewById(R.id.url_et)
 
-            String url = getUrl();
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse(uri_data));
             QuickAppActivity.this.startActivity(intent);
         }
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        int id = view.getId();
-
-        if (id == R.id.spinner){
-            protocol = adapterView.getItemAtPosition(i).toString();
-        }
-    }
-
-    private String getUrl(){
+    private String getUrl(String packageName){
         String[] protocols = getResources().getStringArray(R.array.protocol);
 
-        if(protocol == protocols[0]) {
+        Log.d("wltx",protocol);
+        Log.d("wltx", Arrays.toString(protocols));
+        if(packageName == null){
+            return null;
+        }
+        if(Objects.equals(protocol, protocols[0])) {
             return "http://hapjs.org/app/" + packageName;
-        } else if (protocol == protocols[1]) {
+        } else if (Objects.equals(protocol, protocols[1])) {
             return "https://hapjs.org/app/" + packageName;
-        } else if (protocol == protocols[2]) {
+        } else if (Objects.equals(protocol, protocols[2])) {
             return "hap://app/" + packageName;
-        } else if (protocol == protocols[3]) {
+        } else if (Objects.equals(protocol, protocols[3])) {
             return "hwfastapp://" + packageName;
         }
+        return null;
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
-
-    @Override
-    public boolean onKey(View view, int i, KeyEvent keyEvent) {
-        return false;
-    }
 }
